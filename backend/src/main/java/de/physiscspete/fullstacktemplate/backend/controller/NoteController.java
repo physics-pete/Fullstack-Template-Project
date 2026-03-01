65d8a5d1a13626a3afa1af2ad0aec758763693f2
Peter Gruszka <git@pgsz.de>
2026-03-01 07:51:48 +0100

    added some more endpoints for creation and deletion

--- backend/src/main/java/de/physiscspete/fullstacktemplate/backend/controller/NoteController.java
+++ backend/src/main/java/de/physiscspete/fullstacktemplate/backend/controller/NoteController.java
@@ -4,17 +4,37 @@ package de.physiscspete.fullstacktemplate.backend.controller;
 import de.physiscspete.fullstacktemplate.api.NotesApi;
 import de.physiscspete.fullstacktemplate.backend.service.NoteService;
 import de.physiscspete.fullstacktemplate.model.Note;
+import de.physiscspete.fullstacktemplate.model.NoteCreated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class NoteController implements NotesApi {
 
     private final NoteService noteService;
+
+    @Override
+    public ResponseEntity<NoteCreated> addNewNote(Note note) {
+        NoteCreated noteCreated = noteService.addNewNote(note);
+
+        URI location = URI.create(noteCreated.getUri());
+
+        return ResponseEntity.created(location).body(noteCreated);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteNote(UUID id) {
+
+        noteService.deleteNote(id);
+
+        return ResponseEntity.noContent().build();
+    }
 
     @Override
     public ResponseEntity<List<Note>> getNotes() {
--- backend/src/main/java/de/physiscspete/fullstacktemplate/backend/service/NoteService.java
+++ backend/src/main/java/de/physiscspete/fullstacktemplate/backend/service/NoteService.java
@@ -1,11 +1,13 @@
 package de.physiscspete.fullstacktemplate.backend.service;
 
 import de.physiscspete.fullstacktemplate.model.Note;
+import de.physiscspete.fullstacktemplate.model.NoteCreated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +15,12 @@ public class NoteService {
 
     public List<Note> findAll() {
         return new ArrayList<>();
+    }
+
+    public NoteCreated addNewNote(Note note) {
+        return new NoteCreated("http://localhost:8080/notes/a-b-c-d");
+    }
+
+    public void deleteNote(UUID id) {
     }
 }
